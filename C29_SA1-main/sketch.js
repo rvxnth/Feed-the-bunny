cf3e37bb8bbe01720e4432f6f421c88e6a181c5e
rvxnth <116728504+rvxnth@users.noreply.github.com>
2023-02-15 17:50:05 +0530

    Add files via upload

---
+++ C29_SA1-main/sketch.js
@@ -0,0 +1,49 @@
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
+
+var rope;
+var fruit;
+var fruitConnect;
+
+let engine;
+let world;
+var ground;
+
+function setup() 
+{
+  createCanvas(500,700);
+  frameRate(80);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(200,680,600,20);
+  rope = new Rope(6, {x:245,y:30})
+  fruit = Bodies.circle(300, 300, 20)
+  Matter.Composite.add(rope.body,fruit)
+  fruitConnect = new Link(rope,fruit)
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+  textSize(50)
+
+
+  
+}
+
+function draw() 
+{
+  background(51);
+  ground.show();
+  rope.show();
+  ellipse(fruit.position.x,fruit.position.y,20,20);
+  
+  Engine.update(engine);
+  
+
+ 
+   
+}
